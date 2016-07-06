eea3b793d1b75064296c652dde1c4fd9891be5d0
LiShunRong <18235444249@163.com>
2016-07-06 10:52:35 +0800

    asdasdasd

--- app/src/main/java/com/example/admin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.admin.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public  void click(View v){
+        Toast.makeText(this,"dasdas",Toast.LENGTH_SHORT).show();
     }
 }
